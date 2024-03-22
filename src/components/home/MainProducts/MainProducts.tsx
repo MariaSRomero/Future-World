6a38539f45932fbfda6ed3b8706700097be73e57
MariaSRomero <s8romeromariadolores781@gmail.com>
2024-03-22 14:52:54 -0600

    3_6 Manejo de errores

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,28 +1,27 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
-
 const getProducts = async () => {
-try {
-    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}admin/api/2023-10/products.json`, {
-        headers: new Headers({
-            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+    try {
+        const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}admin/api/2023-10/products.json`, {
+            headers: new Headers({
+                'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+            })
         })
-    }) 
-    const { products } = await response.json()
+        throw new Error('Error')
+        const { products } = await response.json()
         return products
     } catch (error) {
         console.log(error);
     }
 }
-
 export const MainProducts = async () => {
-    const products = await getProducts()
-
+    const products = await getProducts();
+    console.log(products)
     return (
         <section className={styles.MainProducts}>
             <h3>✨ New products released!</h3>
             <div className={styles.MainProducts__grid}>
-                {products?.map((product) => {
+                {products.map((product) => {
                     const imageSrc = product.images[0].src;
                     return (
                         <article key={product.id}>
