3ac21698b8286ff4ea4833d69101e52a57264ea8
MariaSRomero <s8romeromariadolores781@gmail.com>
2024-03-22 14:39:30 -0600

    3_5 Route grouping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import {Roboto} from 'next/font/google'
 import { Header } from "app/components/shared/Header"
 import { Footer } from "app/components/shared/Footer"
 import "app/sass/globals.sass"
+import { Description } from 'app/components/home/Description'
+import { Hero } from 'app/components/home/Hero'
 
 const roboto = Roboto({
   weight: ["100", "300", "500", "700" ],
@@ -17,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Header/>
+
         {children}
         <Footer/>
       </body>
