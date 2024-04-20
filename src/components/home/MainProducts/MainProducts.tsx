80afa9b42c87e7e849a2a4eb59bb56de7f137ddb
MariaSRomero <s8romeromariadolores781@gmail.com>
2024-04-19 23:36:02 -0600

    4_5 Manejando rutas con route handlers

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 import styles from './MainProducts.module.sass'
 
 import { getProducts } from "app/services/shopify"
+
 export const MainProducts = async () => {
-    const products = await getProducts()
+    const response = await fetch('http://localhost:3000/api')
+    const {products} = await response.json()
     
     return(
         <section className={styles.MainProducts}>
