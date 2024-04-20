13d81fc3a4e77ce2527eaf3663bcc18fb3de9fdc
MariaSRomero <s8romeromariadolores781@gmail.com>
2024-04-20 02:27:38 -0600

    4_10 Pagina de producto y arreglos del sitio

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,25 +1,26 @@
 import { getCollections } from "app/services/shopify/collections"
 import Link from "next/link"
-import styles from './StoreLayout.module.sass'
+
+interface Collection {
+    id: string;
+    handle: string;
+    title: string;
+}
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-    const collections = await getCollections()
-
+    const collections: Collection[] = await getCollections();
     return (
-        <main className={styles.StoreLayout}>
-            <h1>Explore</h1>
+        <main>
             <nav>
-                <ul className={styles.StoreLayout__list}>
-                    {
-                        collections.map((collection) => (
-                            <Link key={collection.id} href={'/store/' + collection.handle} className={styles.StoreLayout__chip}>
-                                {collection.title}
-                            </Link>
-                        ))
-                    }
-                </ul>
+                {
+                    collections.map((collection) => (
+                        <Link key={collection.id} href={'/store/' + collection.handle}>
+                            {collection.title}
+                        </Link>
+                    ))
+                }
             </nav>
             {children}
         </main>
-    )
+    );
 }
