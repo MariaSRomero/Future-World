3a575c8b605e04b48fc598f44ce9f87b58b33a91
MariaSRomero <s8romeromariadolores781@gmail.com>
2024-04-20 01:55:51 -0600

    4_8 Filtrando categorias de productos

--- src/app/store/[[...category]]/page.tsx
+++ src/app/store/[[...category]]/page.tsx
@@ -1,20 +1,26 @@
+import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
+import { getCollectionsProducts, getCollections } from "app/services/shopify/collections"
 import { getProducts } from "app/services/shopify/products"
 
-
-interface CategoryProps{
+interface CategoryProps {
     params: {
-        categories: string [],
-        searchParams: string 
+    categories: string[],
     }
+    searchParams?: string
 }
-export default async function Category(props: CategoryProps){
+export default async function Category(props: CategoryProps) {
+    const { categories } = props.params
 
-    const products = await getProducts()
+    let products = []
+    const collections = await getCollections()
 
-    const {categories} = props.params 
-    // throw new Error('Error:BOOM!')
-
-    return(
-        <h1>Categoria dinamica:{categories}</h1>
+    if (categories?.length > 0) {
+        const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
+        products = await getCollectionsProducts(selectedCollectionId)
+    }else {
+        products = await getProducts()
+    }
+    return (
+        <ProductsWrapper products={products} />
     )
 }
